3610657198bcaea803fecdb14c99d979e45347a4
almuallem <almuallem.ali@gmail.com>
2021-11-15 17:27:50 -0700

    NewMessage

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.*;
@@ -29,19 +32,25 @@ public class Main extends Application {
 	
 	//Creating the back button 
     Button backButton = new Button("<-- Back");
-    Label messagesLabel = new Label("Messages" );
-    Button newMessageButton = new Button ("New Message");
-    TextArea messages = new TextArea();
+    Label messagesLabel = new Label("New messages" );
+    Button sendButton = new Button ("Send!");
+    TextArea message = new TextArea();
+	TextField to = new TextField("Type the to address");
+	Label recipientLabel = new Label ("To: ");
+	
+	
+	final ComboBox recipients = new ComboBox();
 	
 	public void start(Stage primaryStage) {
 		try {
 			
 			
-           
+			recipients.getItems().addAll("Dr.1", "Dr.2");
+			
             messagesLabel.setFont(Font.font(24));
             
             //Creating a text area for messages
-            messages.setText("11/12/2021 13:00. From: Doctor. Janaka\nHello patient!"
+            message.setText("11/12/2021 13:00. From: Doctor. Janaka\nHello patient!"
             		+ "You're doing great! The test results came just fine. Cheers!\n\n"
             		+ "10/28/2021 10:00. From: Steve (RN)\n"
             		+ "Hello Sandy. Doc. Janaka is supposed to see you today. thanks!\n\n"
@@ -58,43 +67,40 @@ public class Main extends Application {
             		+ "10/28/2021 10:00. From: Steve (RN)\n"
             		+ "Hello Sandy. Doc. Janaka is supposed to see you today. thanks!\n\n");
             
-            messages.setEditable(false); //Setting the textArea to not be editable.
-            messages.setMinHeight(350); //Setting a minumum height for the text area.
+            message.setEditable(true); //Setting the textArea to not be editable.
+            message.setMinHeight(300); //Setting a minumum height for the text area.
             
             //2.		Instantiating the panes
             //Instantiating the VBOX for the Messages Class. This is the ROOT pane.
             VBox messagesPortal = new VBox();
             
             //Instantiating the top part as an Hbox
-            HBox topPart = new HBox();
-            topPart.setPrefHeight(100);
-            topPart.getChildren().add(backButton);
+            HBox toBox = new HBox();
+            //toBox.setPrefHeight(100);
+            toBox.getChildren().add(recipientLabel);
+            toBox.getChildren().add(recipients);
+            toBox.setMargin(recipientLabel, new Insets(20,20,20, 20));
+            toBox.setMargin(recipients, new Insets(20,20,20, 20));
+
+            //Instantiating the message part as an Hbox
+            HBox messagePart = new HBox();
+            //messagePart.setPrefHeight(100);
+            messagePart.getChildren().add(message);
             
-            //Instantiating the middle part as an Hbox
-            HBox middlePart = new HBox();
-            middlePart.setPrefHeight(100);
-            middlePart.getChildren().add(messagesLabel);
-            
-            //An empty pane in the horizontally centered at the middle vertical pane.
-            HBox middle = new HBox();
-            middle.setMinWidth(550);
-            
-            //Adding the middle part to the middle pane
-            middlePart.getChildren().add(middle);
-            middlePart.getChildren().add(newMessageButton);
             
 
 
             //Setting up margins for the main pane
-            messagesPortal.setMargin(topPart, new Insets(20, 20, 20, 20));  
-            messagesPortal.setMargin(middlePart, new Insets(20, 20, 20, 20));
-            messagesPortal.setMargin(messages, new Insets(20, 20, 20, 20)); 
-            
+            messagesPortal.setMargin(backButton, new Insets(20, 20, 20, 20));  
+            messagesPortal.setMargin(toBox, new Insets(20, 20, 20, 20));
+            messagesPortal.setMargin(messagePart, new Insets(20, 20, 20, 20)); 
+            messagesPortal.setMargin(sendButton, new Insets(20, 20, 20, 20)); 
             
             //Adding the nodes to the main pane
-            messagesPortal.getChildren().add(topPart);
-            messagesPortal.getChildren().add(middlePart);
-            messagesPortal.getChildren().add(messages);
+            messagesPortal.getChildren().add(backButton);
+            messagesPortal.getChildren().add(toBox);
+            messagesPortal.getChildren().add(messagePart);
+            messagesPortal.getChildren().add(sendButton);
 
             
             //Setting up the spacing
@@ -103,7 +109,7 @@ public class Main extends Application {
             
              
             Scene scene = new Scene(messagesPortal,width,height);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
             
@@ -125,9 +131,9 @@ public class Main extends Application {
 					System.out.print("Back Button\n");
 				}
 				
-				else if (source == newMessageButton)
+				else if (source == sendButton)
 				{
-					System.out.print("New Message\n");
+					System.out.print("sendButton\n");
 				}
 			}
 		}
@@ -135,7 +141,7 @@ public class Main extends Application {
 		
 		//Registering the event handler
 		backButton.setOnAction(new ButtonHandler());
-		newMessageButton.setOnAction(new ButtonHandler());
+		sendButton.setOnAction(new ButtonHandler());
 	}
 	
 	
